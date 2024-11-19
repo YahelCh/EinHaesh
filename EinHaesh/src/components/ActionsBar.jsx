8a8d442213f5a6ceb00d9391d36c8ac9197bac37
tovazilber <tovazilber737@gmail.com>
2024-11-19 17:34:16 +0200

    add fireDivuach

--- EinHaesh/src/components/ActionsBar.jsx
+++ EinHaesh/src/components/ActionsBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import iconDivuach from '../assets/speech-bubble.png'
+import fireDivuach from '../assets/fire.jpg'
 
 const ActionsBar = ({ setActiveAction }) => {
 
 
 
     return <div className='actions-bar'>
+        <div className='action' onClick={() => setActiveAction({ name: 'fire', icon: fireDivuach, id:1 })}>
+            <img src={fireDivuach} alt="fire" style={{ width: '32px', height: '32px' }} /></div>
         <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
             <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
         <div className='action'></div>
