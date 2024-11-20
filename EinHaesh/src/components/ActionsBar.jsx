30e56febac99f3d8b5fc03c091e6611655a36caa
CY19xx <YahelCh@users.noreply.github.com>
2024-11-20 12:18:14 +0000

    add fire ans smoke signs

--- EinHaesh/src/components/ActionsBar.jsx
+++ EinHaesh/src/components/ActionsBar.jsx
@@ -9,7 +9,7 @@ const ActionsBar = ({ setActiveAction }) => {
     return <div className='actions-bar'>
         <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
             <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
-        <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
+        <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconLechudim })}>
             <img src={iconLechudim} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
         <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
             <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
@@ -17,10 +17,12 @@ const ActionsBar = ({ setActiveAction }) => {
             <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
         <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
             <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
-        <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
-            <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
-        <div className='action' onClick={() => setActiveAction({ name: 'updtae', icon: iconDivuach })}>
-            <img src={iconDivuach} alt="speech-bubble" style={{ width: '32px', height: '32px' }} /></div>
+        <div className='action' onClick={() => setActiveAction({ name: 'smoke', icon: iconDivuach })}>
+        <div>עשן</div>
+            </div>
+        <div className='action' onClick={() => setActiveAction({ name: 'fire', icon: iconDivuach })}>
+            <div>אש</div>
+           </div>
 
     </div>
 }
--- EinHaesh/src/components/Map.jsx
+++ EinHaesh/src/components/Map.jsx
@@ -31,25 +31,122 @@ const Map = () => {
 
   ];
 
+  const [zones, setZones] = useState([{
+    desc: 'כניסה',
+    id: 0,
+    coords: [
+      [653.3333435058594, 339.33331298828125],
+      [589.3333435058594, 339.33331298828125],
+      [581.3333435058594, 285.33331298828125],
+      [431.3333435058594, 282.33331298828125],
+      [433.3333435058594, 434.33331298828125],
+      [586.3333435058594, 437.33331298828125],
+      [589.3333435058594, 385.33331298828125],
+      [651.3333435058594, 384.33331298828125],
+    ],
+  },
+  // {
+  //   desc: 'מכבסה',
+  //   id: 1,
+  //   coords: [
+  //     [51.505, -0.09],
+  //     [51.51, -0.1],
+  //     [51.51, -0.08],
+  //   ],
+  // },
+  {
+    desc: 'בית כנסת',
+    id: 1,
+    coords: [
+      [586, 497],
+      [429, 492],
+      [424, 634],
+      [588, 637],
+    ],
+  },
+  {
+    desc: 'מטבח',
+    id: 2,
+    coords: [
+      [51.505, -0.09],
+      [51.51, -0.1],
+      [51.51, -0.08],
+    ],
+  },
+  {
+    desc: 'תא 1',
+    id: 3,
+    coords: [
+      [51.505, -0.09],
+      [51.51, -0.1],
+      [51.51, -0.08],
+    ],
+  },
+  {
+    desc: 'תא 2',
+    id: 4,
+    coords: [
+      [51.505, -0.09],
+      [51.51, -0.1],
+      [51.51, -0.08],
+    ],
+  },
+  {
+    desc: 'תא 3',
+    id: 5,
+    coords: [
+      [51.505, -0.09],
+      [51.51, -0.1],
+      [51.51, -0.08],
+    ],
+  },
+
+  {
+    id: 6,
+    coords: [
+      [51.49, -0.1],
+      [51.5, -0.12],
+      [51.5, -0.09],
+    ],
+  },
+  ]);
+
   const MapClickHandler = () => {
     const map = useMapEvents({
       click(event) {
-        if (activeAction && activeAction.icon) { // בדוק אם יש אייקון פעיל
+        if (activeAction && activeAction.icon && !(['fire', 'smoke'].includes(activeAction.name))) { // בדוק אם יש אייקון פעיל
           const newMarker = event.latlng; // מיקום הלחיצה
           console.log(event.latlng);
 
           const icon = L.icon({
-            iconUrl: iconDivuach, // השתמש באייקון שהמשתמש בחר
+            iconUrl: activeAction.icon, // השתמש באייקון שהמשתמש בחר
             iconSize: [32, 32], // גודל האייקון
             iconAnchor: [16, 32], // עוגן האייקון (המיקום שבו יושבת ה"ראש" של האייקון)
             popupAnchor: [0, -32], // מיקום הפופאפ ביחס לאייקון
           });
           setMarkers((prevMarkers) => [...prevMarkers, { position: newMarker, icon }]);
+
         }
       }
     });
   };
 
+  const handleZoneClick = (zoneIndex) => {
+    if (activeAction.name == 'fire') {
+      setZones(prev => {
+        let temp = [...prev];
+        temp[zoneIndex].color = 'rgba(255, 0, 0, 0.5)';
+        return temp;
+      })}
+      if(activeAction.name == 'smoke'){
+        setZones(prev => {
+          let temp = [...prev];
+          temp[zoneIndex].color = '#333333';
+          return temp;
+        })
+    }
+
+  }
 
   return (
     <>
@@ -65,18 +162,41 @@ const Map = () => {
           bounds={bounds}
         />
         {markers.map((marker, index) => (
-          <Marker key={index} position={marker.position}>
+          <Marker key={index} {...marker}>
             <Popup>{marker.popup}</Popup>
           </Marker>
         ))}
 
-        {/* <Polyline positions={path} color="blue" /> */}
+        {zones.map((zone, index) => <Polygon
+          key={zone.id}
+          positions={zone.coords}
+          fillColor={zone.color}
+          pathOptions={{
+
+            color: zone.color || 'transparent',
+            fillOpacity: zone.color ? 0.5 : 0,
+            weight: 1, // עובי הגבול
+          }}
+          eventHandlers={{
+            click: () => handleZoneClick(index),
+          }}
+        />)}
+
+        {/* <Polyline positions={path} color="blue" />
         <Polygon
-          positions={smokeZone} // קואורדינטות הפוליגון
+          positions={zones[1].coords} // קואורדינטות הפוליגון
           color="red" // צבע הגבול
           fillColor="rgba(255, 0, 0, 0.5)" // צבע המילוי עם שקיפות
           weight={2} // רוחב הגבול
         />
+
+        <Polygon
+          positions={zones[0].coords} // קואורדינטות הפוליגון
+          color="red" // צבע הגבול
+          fillColor="rgba(255, 0, 0, 0.5)" // צבע המילוי עם שקיפות
+          weight={2} // רוחב הגבול
+        /> */}
+
         <MapClickHandler />
       </MapContainer>
       {/* <MapLegend/> */}
@@ -85,3 +205,6 @@ const Map = () => {
 };
 
 export default Map;
+
+
+
